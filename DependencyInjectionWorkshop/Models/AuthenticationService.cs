a4d31cfa30b44a5176d7c8ab5c9a4bff8a2d1a6d
infixman <infixman@gmail.com>
2019-06-29 17:19:29 +0800

    NotificationDecorator抽成新檔案

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -2,34 +2,6 @@
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class NotificationDecorator : IAuthentication
-    {
-        private readonly IAuthentication _authentication;
-        private static INotification _notification;
-
-        public NotificationDecorator(IAuthentication authentication, INotification notification)
-        {
-            _authentication = authentication;
-            _notification = notification;
-        }
-
-        public static void PushMsg(string account)
-        {
-            _notification.PushMessage(account);
-        }
-
-        public bool Verify(string account, string password, string otp)
-        {
-            var isValid = _authentication.Verify(account, password, otp);
-            if (!isValid)
-            {
-                _notification.PushMessage(account);
-            }
-
-            return isValid;
-        }
-    }
-
     public class AuthenticationService : IAuthentication
     {
         private readonly IProfile _profile;
---
+++ DependencyInjectionWorkshop/Models/NotificationDecorator.cs
@@ -0,0 +1,30 @@
+﻿namespace DependencyInjectionWorkshop.Models
+{
+    public class NotificationDecorator : IAuthentication
+    {
+        private readonly IAuthentication _authentication;
+        private static INotification _notification;
+
+        public NotificationDecorator(IAuthentication authentication, INotification notification)
+        {
+            _authentication = authentication;
+            _notification = notification;
+        }
+
+        public static void PushMsg(string account)
+        {
+            _notification.PushMessage(account);
+        }
+
+        public bool Verify(string account, string password, string otp)
+        {
+            var isValid = _authentication.Verify(account, password, otp);
+            if (!isValid)
+            {
+                _notification.PushMessage(account);
+            }
+
+            return isValid;
+        }
+    }
+}
