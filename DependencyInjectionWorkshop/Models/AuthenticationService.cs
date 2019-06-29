db04e9d51cdabbe7e44f8333de52cf86a5965db2
infixman <infixman@gmail.com>
2019-06-29 10:33:04 +0800

    新增 Verify 需求 (進入後馬上檢查帳號是否被鎖)
    登入時先打一次API檢查帳號是被鎖

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -13,6 +13,15 @@ namespace DependencyInjectionWorkshop.Models
     {
         public bool Verify(string account, string password, string otp)
         {
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var isLockedResponse = httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", account).Result;
+            isLockedResponse.EnsureSuccessStatusCode();
+
+            if (isLockedResponse.Content.ReadAsAsync<bool>().Result)
+            {
+                throw new FailedTooManyTimesException();
+            }
+
             string pwdHashFromDb;
             using (var connection = new SqlConnection("my connection string"))
             {
@@ -30,7 +39,6 @@ namespace DependencyInjectionWorkshop.Models
             var pwdHashFromInput = hash.ToString();
 
             string otpFromApi;
-            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
             var response = httpClient.PostAsJsonAsync("api/otps", account).Result;
             if (response.IsSuccessStatusCode)
             {
@@ -57,5 +65,9 @@ namespace DependencyInjectionWorkshop.Models
                 return false;
             }
         }
+    }
+
+    public class FailedTooManyTimesException : Exception
+    {
     }
 }
