729d46d957a0e425c5efcaf2f73591897c46e672
infixman <infixman@gmail.com>
2019-06-29 13:48:51 +0800

    其餘class都比照Ilogger一樣抽介面，並且field改名為合適的名字

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -4,30 +4,30 @@ namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
-        private readonly ProfileDao _profileDao;
-        private readonly Sha256Adapter _sha256Adapter;
-        private readonly OtpService _otpService;
-        private readonly FailedCounter _failedCounter;
-        private readonly SlackAdapter _slackAdapter;
+        private readonly IProfile _profile;
+        private readonly IHash _hash;
+        private readonly IOtpService _otpService;
+        private readonly IFailedCounter _failedCounter;
+        private readonly INotification _notification;
         private readonly ILogger _logger;
 
-        public AuthenticationService(ProfileDao profileDao, Sha256Adapter sha256Adapter, OtpService otpService, FailedCounter failedCounter, SlackAdapter slackAdapter, ILogger logger)
+        public AuthenticationService(IProfile profile, IHash hash, IOtpService otpService, IFailedCounter failedCounter, INotification notification, ILogger logger)
         {
-            _profileDao = profileDao;
-            _sha256Adapter = sha256Adapter;
+            _profile = profile;
+            _hash = hash;
             _otpService = otpService;
             _failedCounter = failedCounter;
-            _slackAdapter = slackAdapter;
+            _notification = notification;
             _logger = logger;
         }
 
         public AuthenticationService()
         {
-            _profileDao = new ProfileDao();
-            _sha256Adapter = new Sha256Adapter();
+            _profile = new ProfileDao();
+            _hash = new Sha256Adapter();
             _otpService = new OtpService();
             _failedCounter = new FailedCounter();
-            _slackAdapter = new SlackAdapter();
+            _notification = new SlackAdapter();
             _logger = new NLogAdapter();
         }
 
@@ -40,10 +40,10 @@ namespace DependencyInjectionWorkshop.Models
             }
 
             //從DB撈使用者密碼
-            var pwdFromDb = _profileDao.GetPassword(account);
+            var pwdFromDb = _profile.GetPassword(account);
 
             //將使用者輸入的密碼HASH一下
-            var hashPwd = _sha256Adapter.Hash(password);
+            var hashPwd = _hash.Hash(password);
 
             //從API取得目前的OTP
             var otpFromApi = _otpService.GetCurrentOtp(account);
@@ -58,7 +58,7 @@ namespace DependencyInjectionWorkshop.Models
             else //驗證失敗
             {
                 //打SLACK通知使用者
-                _slackAdapter.PushMessage(account);
+                _notification.PushMessage(account);
 
                 //增加錯誤次數
                 _failedCounter.AddFailedCount(account);
