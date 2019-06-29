8bfbb1953d793bb7bbf78ad30fb9b684faa58fc8
infixman <infixman@gmail.com>
2019-06-29 14:35:51 +0800

    完成第一個測試

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using DependencyInjectionWorkshop.Models;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -9,7 +10,21 @@ namespace DependencyInjectionWorkshopTests
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+            var profile = Substitute.For<IProfile>();
+            var hash = Substitute.For<IHash>();
+            var otpService = Substitute.For<IOtpService>();
+            var failedCounter = Substitute.For<IFailedCounter>();
+            var notification = Substitute.For<INotification>();
+            var logger = Substitute.For<ILogger>();
+
+            profile.GetPassword("joey").ReturnsForAnyArgs("abc");
+            hash.Compute("9487").ReturnsForAnyArgs("abc");
+            otpService.GetCurrentOtp("joey").ReturnsForAnyArgs("9527");
+
+            var authenticationService = new AuthenticationService(profile, hash, otpService,
+                failedCounter, notification, logger);
+
+            Assert.IsTrue(authenticationService.Verify("joey", "9487", "9527"));
         }
     }
 }
