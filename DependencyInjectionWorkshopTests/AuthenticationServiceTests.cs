3ec588265211f11d2562cdb63ce78d3e51880baf
infixman <infixman@gmail.com>
2019-06-29 15:01:00 +0800

    test class 新增 SetUp method
    因為不同測試框架對於 class 調用時的行為不同，如果Field在宣告的時候就有初始化，有的框架會一直keep這個field的狀態，而有的框架則是每次都會初始化一次，所以建議還是把初始化寫在SetUp裡面

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -7,24 +7,39 @@ namespace DependencyInjectionWorkshopTests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        private IProfile _profile;
+        private IHash _hash;
+        private IOtpService _otpService;
+        private IFailedCounter _failedCounter;
+        private INotification _notification;
+        private ILogger _logger;
+        private AuthenticationService _authenticationService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _profile = Substitute.For<IProfile>();
+            _hash = Substitute.For<IHash>();
+            _otpService = Substitute.For<IOtpService>();
+            _failedCounter = Substitute.For<IFailedCounter>();
+            _notification = Substitute.For<INotification>();
+            _logger = Substitute.For<ILogger>();
+
+            _authenticationService = 
+                new AuthenticationService(_profile, _hash, _otpService, _failedCounter, 
+                    _notification, _logger);
+        }
+
+
         [Test]
         public void is_valid()
         {
-            var profile = Substitute.For<IProfile>();
-            var hash = Substitute.For<IHash>();
-            var otpService = Substitute.For<IOtpService>();
-            var failedCounter = Substitute.For<IFailedCounter>();
-            var notification = Substitute.For<INotification>();
-            var logger = Substitute.For<ILogger>();
+            _profile.GetPassword("joey").ReturnsForAnyArgs("abc");
+            _hash.Compute("9487").ReturnsForAnyArgs("abc");
+            _otpService.GetCurrentOtp("joey").ReturnsForAnyArgs("9527");
 
-            profile.GetPassword("joey").ReturnsForAnyArgs("abc");
-            hash.Compute("9487").ReturnsForAnyArgs("abc");
-            otpService.GetCurrentOtp("joey").ReturnsForAnyArgs("9527");
-
-            var authenticationService = new AuthenticationService(profile, hash, otpService,
-                failedCounter, notification, logger);
-
-            Assert.IsTrue(authenticationService.Verify("joey", "9487", "9527"));
+            var isValid = _authenticationService.Verify("joey", "9487", "9527");
+            Assert.IsTrue(isValid);
         }
     }
 }
